007cf105799afeec8728e68f25b0bdf43b969c81
Abu Bakar <138878660+AbuBakarPH@users.noreply.github.com>
2023-08-09 18:04:08 +0500

    payment methods fixed

--- pages/menu.jsx
+++ pages/menu.jsx
@@ -8,124 +8,109 @@ import { getMealsByRestaurant } from "../services/menu.api";
 import { Box, LoadingOverlay, Text } from "@mantine/core";
 import { useDebouncedValue } from "@mantine/hooks";
 import { toast } from "react-toastify";
-import axios from "axios";
 
 export default function Menu() {
-	const [loading, setLoading] = useState(true);
-	const categories = [
-		"All",
-		...new Set(data.meals.map((item) => item.category)),
-	];
-	const [mealsInfo, setMealsInfo] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const categories = [
+    "All",
+    ...new Set(data.meals.map((item) => item.category)),
+  ];
+  const [mealsInfo, setMealsInfo] = useState([]);
+  const [activeCategory, setActiveCategory] = useState(null);
+  const [search, setSearch] = useState("");
+  const [debouncedSearch] = useDebouncedValue(search, 1000);
 
-	const [activeCategory, setActiveCategory] = useState(null);
-	const [search, setSearch] = useState("");
-	const [debouncedSearch] = useDebouncedValue(search, 1000);
+  const handleFetchMenu = async () => {
+    try {
+      setLoading(true);
+      getMealsByRestaurant({ name: debouncedSearch }).then(({ data }) => {
+        setMealsInfo(data.results);
+      });
+      const { data } = await getMealsByRestaurant({ name: debouncedSearch });
+      setMealsInfo(data.results);
+    } catch (error) {
+      toast.error(error?.message || error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-	useEffect(() => {
-		axios.get(`http://localhost:3000/v1/meals/${process.env.NEXT_PUBLIC_RESTAURANT_ID}`).then((response) => {
-			setMealsInfo(response.data.results);
-			setLoading(false);
-		});
-	  }, [])
+  useEffect(() => {
+    handleFetchMenu();
+  }, []);
 
-	
+  useEffect(() => {
+    handleFetchMenu();
+  }, [debouncedSearch]);
 
-	// const handleFetchMenu = async () => {
-	// 	try {
-	// 		setLoading(true);
-	// 		getMenu().then(({ data }) => {
-	// 			setMealsInfo(data.results);
-	// 		});
-	// 		const { data } = await getMenu();
-	// 		setMealsInfo(data.results);
-	// 	} catch (error) {
-	// 		toast.error(error?.message || error);
-	// 	} finally {
-	// 		setLoading(false);
-	// 	}
-	// };
+  const filteredMeals = useMemo(() => {
+    return mealsInfo.filter((meal) => {
+      if (activeCategory === "All" || !activeCategory) return true;
+      return (
+        (!activeCategory || activeCategory === meal.category) &&
+        meal.name.includes(search)
+      );
+    });
+  }, [mealsInfo, activeCategory, search]);
 
-	// useEffect(() => {
-	// 	handleFetchMenu();
-	// }, []);
+  return (
+    <Layout title="MPO Menu">
+      <div className="bg-white">
+        <div className="container mx-auto mb-4">
+          <div className="flex bg-yellow-300 p-3 mt-5 items-center">
+            <FaMapMarkerAlt size={25} />
+            <p className="ml-2">Delivering To 14145 Ikorodu Close, Lagos</p>
+          </div>
+          <div className="mt-3 mb-5 flex relative md:mx-0">
+            <input
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              type="text"
+              placeholder="Search for meals..."
+              className=" hover:shadow-lg border w-full py-3 px-4 rounded outline-none bg-gray-200"
+            />
+            <FaSearch
+              size={25}
+              className="text-blue-600 absolute right-5 top-3"
+            />
+          </div>
 
-	// useEffect(() => {
-	// 	handleFetchMenu();
-	// }, [debouncedSearch]);
-
-	useEffect(() => {
-		handleFetchMenu();
-	}, [debouncedSearch]);
-
-	const filteredMeals = useMemo(() => {
-		return mealsInfo.filter((meal) => {
-			if (activeCategory === "All" || !activeCategory) return true;
-			return (
-				(!activeCategory || activeCategory === meal.category) &&
-				meal.name.includes(search)
-			);
-		});
-	}, [mealsInfo, activeCategory, search]);
-
-	return (
-		<Layout title="MPO Menu">
-			<div className="bg-white">
-				<div className="container mx-auto mb-4">
-					<div className="flex bg-yellow-300 p-3 mt-5 items-center">
-						<FaMapMarkerAlt size={25} />
-						<p className="ml-2">Delivering To 14145 Ikorodu Close, Lagos</p>
-					</div>
-					<div className="mt-3 mb-5 flex relative md:mx-0">
-						<input
-							value={search}
-							onChange={(e) => setSearch(e.target.value)}
-							type="text"
-							placeholder="Search for meals..."
-							className=" hover:shadow-lg border w-full py-3 px-4 rounded outline-none bg-gray-200"
-						/>
-						<FaSearch
-							size={25}
-							className="text-blue-600 absolute right-5 top-3"
-						/>
-					</div>
-
-					<div className="gap-4 grid grid-cols-1 lg:grid-cols-4 mx-4">
-						<div className="w-full col-span-1">
-							<div className="bg-white  border p-5 rounded shadow-md w-full">
-								<Category
-									categories={categories}
-									activeCategory={activeCategory}
-									setActiveCategory={(category) => setActiveCategory(category)}
-								/>
-							</div>
-						</div>
-						<div className="relative col-span-1 lg:col-span-3">
-							<LoadingOverlay visible={loading} overlayBlur={2} />
-							{filteredMeals && filteredMeals.length ? (
-								<div className="w-full py-5 md:py-6 md:pt-0 rounded mb-20">
-									<ul
-										role="list"
-										className="space-y-1 sm:grid grid-cols-2 lg:grid-cols-3 sm:gap-x-6 sm:gap-y-8 sm:space-y-0 lg:gap-x-8"
-									>
-										{filteredMeals.map((meal, index) => (
-											<MealItem meal={meal} key={index} />
-										))}
-									</ul>
-								</div>
-							) : (
-								<div className="col-span-3 w-full h-full flex items-center justify-center flex-col text-md">
-									<Text align="center">No results found</Text>
-									<Text c="dimmed">
-										Look like there is no meal that match your criteria, please
-										try another one.
-									</Text>
-								</div>
-							)}
-						</div>
-					</div>
-				</div>
-			</div>
-		</Layout>
-	);
+          <div className="gap-4 grid grid-cols-1 lg:grid-cols-4 mx-4">
+            <div className="w-full col-span-1">
+              {/* <div className="bg-white border p-5 rounded shadow-md w-full"> */}
+              <Category
+                categories={categories}
+                activeCategory={activeCategory || "All"}
+                setActiveCategory={(category) => setActiveCategory(category)}
+              />
+              {/* </div> */}
+            </div>
+            <div className="relative col-span-1 lg:col-span-3">
+              <LoadingOverlay visible={loading} overlayBlur={2} />
+              {filteredMeals && filteredMeals.length ? (
+                <div className="w-full py-5 md:py-6 md:pt-0 rounded mb-20">
+                  <ul
+                    role="list"
+                    className="space-y-1 sm:grid grid-cols-2 lg:grid-cols-3 sm:gap-x-6 sm:gap-y-8 sm:space-y-0 lg:gap-x-8"
+                  >
+                    {filteredMeals.map((meal, index) => (
+                      <MealItem meal={meal} key={index} />
+                    ))}
+                  </ul>
+                </div>
+              ) : (
+                <div className="col-span-3 w-full h-full flex items-center justify-center flex-col text-md">
+                  <Text align="center">No results found</Text>
+                  <Text c="dimmed">
+                    Look like there is no meal that match your criteria, please
+                    try another one.
+                  </Text>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
--- services/menu.api.js
+++ services/menu.api.js
@@ -1,12 +1,14 @@
 import { AuthClient } from "@/utils/client";
 
-export const getMenu = (params) => {
-    return AuthClient().get(`/v1/menus/${process.env.NEXT_PUBLIC_RESTAURANT_ID}/meals`, {
-        params
-    });
+export const getMealsByRestaurant = (params) => {
+  return AuthClient().get(
+    `/v1/restaurants/${process.env.NEXT_PUBLIC_RESTAURANT_ID}/meals`,
+    {
+      params,
+    }
+  );
 };
 
 export const getMealDetail = (item) => {
-    return AuthClient().get(`/v1/meals/${item}`);
-    
+  return AuthClient().get(`/v1/meals/${item}`);
 };
